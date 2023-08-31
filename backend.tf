ac303becb45d5fd1c5bd5a6d45b3f9c3f49b705d
Sahib Gasimov <sahib.gasimov@yahoo.com>
2023-08-31 03:15:57 -0400

    uncommented backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
-terraform {
-  backend "s3" {
-    bucket = "your_s3_bucket"
-    key    = "your_folder/terraform.tfstate"
-    region = "us-east-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket = "your_s3_bucket"
+#    key    = "your_folder/terraform.tfstate"
+#    region = "us-east-1"
+#  }
+#}
